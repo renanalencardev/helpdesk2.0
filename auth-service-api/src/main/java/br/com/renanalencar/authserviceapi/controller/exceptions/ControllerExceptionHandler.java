2963d465a820be37226840f82a2ded1861661594
Renan Alencar <antonio.alencar11@pm.pe.gov.br>
2026-01-21 15:40:43 -0300

    feat: criado o manipulador de excecoes do controller

---
+++ auth-service-api/src/main/java/br/com/renanalencar/authserviceapi/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,52 @@
+package br.com.renanalencar.authserviceapi.controller.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import models.exceptions.ResourceNotFoundException;
+import models.exceptions.StandardError;
+import models.exceptions.ValidationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.*;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    ResponseEntity<StandardError> handleNotFoundException(final UsernameNotFoundException ex,
+                                              final HttpServletRequest request) {
+        return ResponseEntity.status(NOT_FOUND).body(
+                StandardError.builder()
+                            .timestamp(now())
+                            .status(NOT_FOUND.value())
+                            .error(NOT_FOUND.getReasonPhrase())
+                            .message(ex.getMessage())
+                            .path(request.getRequestURI())
+                            .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<StandardError> handleMethodArgumentNotValidException(
+            final MethodArgumentNotValidException ex, final HttpServletRequest request) {
+        var error = ValidationException.builder()
+                .timestamp(now())
+                .status(BAD_REQUEST.value())
+                .error("Exceção de validação")
+                .message("Exceção em atributos de validação")
+                .path(request.getRequestURI())
+                .errors(new ArrayList<>())
+                .build();
+        for(FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(error);
+    }
+}
