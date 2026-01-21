d0e0a1ed937d3c6839996b6481cd3359d6673047
Renan Alencar <antonio.alencar11@pm.pe.gov.br>
2026-01-21 11:09:44 -0300

    feat: criadas as configuracoes de seguranca do compomente de autenticacao

---
+++ auth-service-api/src/main/java/br/com/renanalencar/authserviceapi/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package br.com.renanalencar.authserviceapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public static final String[] SWAGGER_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/api/auth/login/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(http -> http
+                    .requestMatchers(SWAGGER_WHITELIST).permitAll()
+                            .anyRequest().authenticated()
+                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)                )
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
