dabe1d6e78d52a86097617149de2d713a4211b41
Renan Alencar <antonio.alencar11@pm.pe.gov.br>
2026-01-23 15:26:12 -0300

    refactor: implementando a chamada da autenticacao jwt no controller de autenticacao

--- auth-service-api/src/main/java/br/com/renanalencar/authserviceapi/controller/impl/AuthControllerImpl.java
+++ auth-service-api/src/main/java/br/com/renanalencar/authserviceapi/controller/impl/AuthControllerImpl.java
@@ -1,18 +1,27 @@
 package br.com.renanalencar.authserviceapi.controller.impl;
 
 import br.com.renanalencar.authserviceapi.controller.AuthController;
+import br.com.renanalencar.authserviceapi.security.dtos.JWTAuthenticationImpl;
+import br.com.renanalencar.authserviceapi.utils.JWTUtils;
+import lombok.RequiredArgsConstructor;
 import models.requests.AuthenticateRequest;
 import models.responses.AuthenticateResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthControllerImpl implements AuthController {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtils jwtUtils;
+
     @Override
-    public ResponseEntity<AuthenticateResponse> authenticate(AuthenticateRequest request) {
-        return ResponseEntity.ok().body(AuthenticateResponse.builder()
-                        .type("Bearer")
-                        .token("token")
-                .build());
+    public ResponseEntity<AuthenticateResponse> authenticate(final AuthenticateRequest request) throws Exception {
+        return ResponseEntity.ok().body(
+                new JWTAuthenticationImpl(jwtUtils, authenticationConfiguration.getAuthenticationManager())
+                        .authenticate(request)
+                );
     }
 }
