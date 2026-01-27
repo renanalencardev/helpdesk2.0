18b816e5632ebbd8671faa6e7ab95dad12907d10
Renan Alencar <antonio.alencar11@pm.pe.gov.br>
2026-01-27 07:44:58 -0300

    refactor: modificado o authenticate response para passar o refresh token

--- hd-commons-lib/src/main/java/models/responses/AuthenticateResponse.java
+++ hd-commons-lib/src/main/java/models/responses/AuthenticateResponse.java
@@ -1,10 +1,13 @@
 package models.responses;
 
 import lombok.Builder;
+import lombok.With;
 
+@With
 @Builder
 public record AuthenticateResponse(
         String token,
-        String type
+        String type,
+        String refreshToken
 ) {
 }
