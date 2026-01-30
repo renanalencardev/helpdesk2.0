1457b9d99e023b9825e2483c55120be882726285
Renan Alencar <antonio.alencar11@pm.pe.gov.br>
2026-01-29 22:12:11 -0300

    feat: criado o objeto de requisicao de ordem de servico

---
+++ hd-commons-lib/src/main/java/models/requests/CreateOderRequest.java
@@ -0,0 +1,29 @@
+package models.requests;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateOderRequest(
+        @Schema(description = "ID do solicitante", example = "na42df7a8sd76fadf871dsf3")
+        @NotBlank(message = "O campo requesterId é obrigatório")
+        @Size(min = 24, max = 36, message = "O campo requesterId deve ter entre 24 e 36 caracteres")
+        String requesterId,
+        @Schema(description = "ID do cliente", example = "cb12df7a8sd76fadf871dsf9")
+        @NotBlank(message = "O campo customerId é obrigatório")
+        @Size(min = 24, max = 36, message = "O campo customerId deve ter entre 24 e 36 caracteres")
+        String customerId,
+        @Schema(description = "Título da ordem", example = "Instalação de software")
+        @NotBlank(message = "O campo title é obrigatório")
+        @Size(min = 3, max = 45, message = "O campo title deve ter entre 3 e 45 caracteres")
+        String title,
+        @Schema(description = "Descrição da ordem", example = "Instalação do software XYZ na máquina do cliente")
+        @NotBlank(message = "O campo description é obrigatório")
+        @Size(min = 10, max = 3000, message = "O campo description deve ter entre 10 e 3000 caracteres")
+        String description,
+        @Schema(description = "Status da ordem", example = "Open")
+        @NotBlank(message = "O campo status é obrigatório")
+        @Size(min = 4, max = 15, message = "O campo status deve ter entre 4 e 15 caracteres")
+        String status
+) {
+}
